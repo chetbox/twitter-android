72e73a5e088bf2f34d994224bf419c9fa4f67933
Chetan Padia <chetan@swiftkey.com>
2015-06-24 19:43:02 +0100

    use Twitter Kit's tweet parsing

--- app/src/main/java/com/chetbox/twitter/android/HomeTimeline.java
+++ app/src/main/java/com/chetbox/twitter/android/HomeTimeline.java
@@ -2,23 +2,20 @@ package com.chetbox.twitter.android;
 
 import android.content.Context;
 import android.os.AsyncTask;
-import android.widget.Toast;
 
+import com.google.gson.Gson;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
-import com.twitter.sdk.android.core.models.Identifiable;
 import com.twitter.sdk.android.core.models.Tweet;
-import com.twitter.sdk.android.core.models.User;
 import com.twitter.sdk.android.tweetui.Timeline;
 import com.twitter.sdk.android.tweetui.TimelineCursor;
 import com.twitter.sdk.android.tweetui.TimelineResult;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 
@@ -27,6 +24,7 @@ import retrofit.client.Response;
 import twitter4j.Paging;
 import twitter4j.Status;
 import twitter4j.TwitterFactory;
+import twitter4j.TwitterObjectFactory;
 import twitter4j.conf.Configuration;
 import twitter4j.conf.ConfigurationBuilder;
 
@@ -36,13 +34,16 @@ public class HomeTimeline implements Timeline<Tweet> {
 
     private final twitter4j.Twitter mTwitter;
     private final Context mContext;
+    private final Gson mGson = new Gson();
 
     public HomeTimeline(Context context) {
         mContext = context;
 
         TwitterSession session = Twitter.getSessionManager().getActiveSession();
 
-        Configuration twitterConfig = new ConfigurationBuilder().setDebugEnabled(true)
+        Configuration twitterConfig = new ConfigurationBuilder()
+                .setDebugEnabled(true)
+                .setJSONStoreEnabled(true)
                 .setOAuthConsumerKey(TwitterApplication.TWITTER_KEY)
                 .setOAuthConsumerSecret(TwitterApplication.TWITTER_SECRET)
                 .setOAuthAccessToken(session.getAuthToken().token)
@@ -80,7 +81,7 @@ public class HomeTimeline implements Timeline<Tweet> {
                 List<twitter4j.Status> tw4jTweets = mTwitter.getHomeTimeline(mPaging);
                 List<Tweet> tweets = new ArrayList<>(tw4jTweets.size());
                 for (twitter4j.Status tw4jTweet : tw4jTweets) {
-                    tweets.add(toFabricTweet(tw4jTweet));
+                    tweets.add(toTwitterKitTweet(tw4jTweet));
                 }
                 return tweets;
             } catch (twitter4j.TwitterException e) {
@@ -109,88 +110,14 @@ public class HomeTimeline implements Timeline<Tweet> {
         return new Response("https://twitter.com", 200, "OK", new ArrayList<Header>(), null);
     }
 
-    private static Tweet toFabricTweet(Status tweet) {
+    private Tweet toTwitterKitTweet(Status tweet) {
         if (tweet == null) {
             return null;
         }
-        return new Tweet(
-                null, // TODO: coordinates
-                DATE_TIME_RFC822.format(tweet.getCreatedAt()),
-                null, // TODO: currentUserRetweet
-                null, // TODO: entities
-                tweet.getFavoriteCount(),
-                tweet.isFavorited(),
-                null, // TODO: filterLevel
-                tweet.getId(),
-                Long.toString(tweet.getId()),
-                tweet.getInReplyToScreenName(),
-                tweet.getInReplyToStatusId(),
-                Long.toString(tweet.getInReplyToStatusId()),
-                tweet.getInReplyToUserId(),
-                Long.toString(tweet.getInReplyToUserId()),
-                tweet.getLang(),
-                null, // TODO: place
-                tweet.isPossiblySensitive(),
-                null, // TODO: scopes
-                tweet.getRetweetCount(),
-                tweet.isRetweeted(),
-                toFabricTweet(tweet.getRetweetedStatus()),
-                tweet.getSource(),
-                tweet.getText(),
-                tweet.isTruncated(),
-                toFabricUser(tweet.getUser()),
-                false, // TODO: withheldCopyright
-                null, // TODO: witheldInCountries,
-                null); // TODO: witheldScope
-    }
 
-    private static User toFabricUser(twitter4j.User user) {
-        if (user == null) {
-            return null;
-        }
-        return new User(
-                user.isContributorsEnabled(),
-                DATE_TIME_RFC822.format(user.getCreatedAt()),
-                user.isDefaultProfile(),
-                user.isDefaultProfileImage(),
-                user.getDescription(),
-                null, // TODO: emailAddress?
-                null, // TODO: entities
-                user.getFavouritesCount(),
-                user.isFollowRequestSent(),
-                user.getFollowersCount(),
-                user.getFriendsCount(),
-                user.isGeoEnabled(),
-                user.getId(),
-                Long.toString(user.getId()),
-                user.isTranslator(),
-                user.getLang(),
-                user.getListedCount(),
-                user.getLocation(),
-                user.getName(),
-                user.getProfileBackgroundColor(),
-                user.getProfileBackgroundImageURL(),
-                user.getProfileBackgroundImageUrlHttps(),
-                user.isProfileBackgroundTiled(),
-                user.getProfileBannerURL(),
-                user.getProfileImageURL(),
-                user.getProfileImageURLHttps(),
-                user.getProfileLinkColor(),
-                user.getProfileSidebarBorderColor(),
-                user.getProfileSidebarFillColor(),
-                user.getProfileTextColor(),
-                user.isProfileUseBackgroundImage(),
-                user.isProtected(),
-                user.getScreenName(),
-                user.isShowAllInlineMedia(),
-                toFabricTweet(user.getStatus()),
-                user.getStatusesCount(),
-                user.getTimeZone(),
-                user.getURL(),
-                user.getUtcOffset(),
-                user.isVerified(),
-                null, // TODO: witheldInCountries,
-                null); // TODO: witheldScope
+        // re-parse as a Twitter Kit object
+        String tweetJson = TwitterObjectFactory.getRawJSON(tweet);
+        return mGson.fromJson(tweetJson, Tweet.class);
     }
 
 }
